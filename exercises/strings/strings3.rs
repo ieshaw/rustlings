8eb05d2e615b1fa29ec6b7b9f6f1c95fb7ae0737
e <e@es-MacBook-Pro.local>
2023-04-10 14:09:54 -0700

    Major progress

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,33 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut s = String::from(input);
+    let ws: char = ' ';
+    while ws == s.chars().next().unwrap() {
+        s.remove(0);
+    }
+    while ws == s.chars().last().unwrap() {
+        s.pop();
+    }
+    return s;
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s = String::from(input);
+    s.push_str(" world!");
+    return s;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let rs = "cars";
+    let mut s = String::from(input);
+    let mut split = s.split_once(rs).unwrap(); 
+    s = format!("{}balloons{}", split.0, split.1);
+    return s;
 }
 
 #[cfg(test)]
